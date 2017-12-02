2e9d9e68f19a02660807c7c583df693f19e1ba82
Francisco Guerreiro <francisco.guerreiro.jr@gmail.com>
2017-12-02 14:30:32 -0500

    Class WasRun

--- xUnit.py
+++ xUnit.py
@@ -7,3 +7,16 @@
 # Run multiple tests
 # Report collected results
 
+# We'll call the test class WasRun, because it's a test case that reports
+# if the method was run
+
+class WasRun:
+    pass
+
+
+
+test = WasRun("testMethod")
+print(test.wasRun)
+
+test.testMethod()
+print(test.wasRun)
